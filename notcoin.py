bb870ae8a16f62ec9344e5e9f86a424a9e4d3efe
sagadmoh <147681439+sagadmoh@users.noreply.github.com>
2024-03-28 17:09:46 +0330

    Update notcoin.py

--- notcoin.py
+++ notcoin.py
@@ -24,7 +24,7 @@ print(os.environ)
 Session_string = os.environ.get('SESSION_STRING')
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
-admin = os.environ.get('ADMIN')
+admin = 804245549
     
 
 VERSION = "1.7"
